3e6e47ce0900232282d27ebc210ca20f200f9cfe
evgeny <ssbscrib@gmail.com>
2024-09-26 20:54:19 +0500

    Add two classes in Storage

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -1,4 +1,35 @@
 package ru.javawebinar.basejava.storage;
 
-public class SortedArrayStorage {
+import ru.javawebinar.basejava.model.Resume;
+
+public class SortedArrayStorage extends AbstractArrayStorage{
+    @Override
+    protected int getIndex(String uuid) {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public void save(Resume r) {
+
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
 }
