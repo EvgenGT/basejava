27d5385db7ba2e15750bd98d51ecda2b388a5068
evgeny <ssbscrib@gmail.com>
2024-11-05 22:06:21 +0500

    HW_4 edit AbstractArrayStorageTest

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -1,9 +1,16 @@
 package ru.javawebinar.basejava.storage;
 
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.basejava.model.Resume;
 
 import static org.junit.jupiter.api.Assertions.*;
-class AbstractArrayStorageTest {
+
+public abstract class AbstractArrayStorageTest {
+    private Storage storage;
+
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
 
     @Test
     void size() {
