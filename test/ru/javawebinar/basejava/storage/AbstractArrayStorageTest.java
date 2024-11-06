0462e292779f10c8dc71c27fa18a06d1b1463599
evgeny <ssbscrib@gmail.com>
2024-11-06 20:52:31 +0500

    HW_4 add @Test void size()

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -14,6 +14,22 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     void size() {
+        Storage storage = new ArrayStorage(); // Используем конкретный класс, реализующий AbstractArrayStorage
+        Resume resume1 = new Resume();
+        Resume resume2 = new Resume();
+        Resume resume3 = new Resume();
+
+        // Установим UUID через сеттер, если он существует
+        resume1.setUuid(UUID_1);
+        resume2.setUuid(UUID_2);
+        resume3.setUuid(UUID_3);
+
+        storage.save(resume1);
+        storage.save(resume2);
+        storage.save(resume3);
+
+        assertEquals(3, storage.size(), "Размер хранилища должен составлять 3 после добавления 3 резюме.");
+
     }
 
     @Test
