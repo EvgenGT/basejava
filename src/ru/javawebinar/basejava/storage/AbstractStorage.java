fd2e91ed1b42456463ced7389883d372d897e14f
evgeny <ssbscrib@gmail.com>
2024-12-11 21:58:48 +0500

    HW_5 add AbstractStorage

---
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -0,0 +1,45 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+public class AbstractStorage implements Storage{
+    protected static final int STORAGE_LIMIT = 10000;
+
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public void save(Resume r) {
+
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        return null;
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
